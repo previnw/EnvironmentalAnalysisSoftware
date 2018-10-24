4e6138ab14d550316509e922c99381a96705bb6e
previnw <previn.wong@sjsu.edu>
2018-10-23 17:50:18 -0700

    all folders

---
+++ index.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+posts = [
+	{
+		'author': 'Previn Wong',
+		'title': 'Blog Post 1',
+		'content': 'First post content',
+		'date_posted': 'September 17, 2018'
+	},
+	{
+		'author': 'Dan P',
+		'title': 'Blog Post 2',
+		'content': 'Second post content',
+		'date_posted': 'September 18, 2018'
+	}
+]
+
+@app.route('/')
+@app.route('/index')
+@app.route('/home')
+def index():
+    return render_template('index.html')
+
+@app.route('/details')
+def details():
+	return render_template('details.html', posts=posts, title='Details')
+
+if __name__ == '__main__':
+	app.run(debug=True)
+
