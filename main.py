5362e00e31e05a1c9ddf74a71a74776b4af19733
previnw <previn.wong@sjsu.edu>
2018-11-24 20:36:58 -0800

    dynamic updates

--- main.py
+++ main.py
@@ -26,11 +26,52 @@ class Humidity:
 	def __init__(self, data_val_hum, type_hum, id_hum):
 		self.id_hum = id_hum
 		self.type_hum = type_hum
-		self.data_val_hum = data_val_hum	
+		self.data_val_hum = data_val_hum
+
+global temp, humi, co, co2, smoke, pressure
+temp = 0
+humi = 0
+co = 0
+co2 = 0
+smoke = 0
+pressure = 0
+
+posts = [
+	{
+		'author': 'Previn Wong',
+		'title': 'Blog Post 1',
+		'content': 'First post content',
+		'date_posted': 'September 17, 2018'
+	},
+	{
+		'author': 'Dan P',
+		'title': 'Blog Post 2',
+		'content': 'Second post content',
+		'date_posted': 'September 18, 2018'
+	},
+	{
+		'author': 'Mike Lee',
+		'title': 'Blog Post 3',
+		'content': 'Third post content',
+		'date_posted': 'September 19, 2018'
+	},
+	{
+		'author': 'Conrad',
+		'title': 'Blog Post 4',	
+		'content': 'Forth post content',
+		'date_posted': 'September 20, 2018'
+	},
+	{
+		'author': 'Josh',
+		'title': 'Blog Post 5',
+		'content': 'Fifth post content',
+		'date_posted': 'September 21, 2018'
+	}
+]
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 @app.route('/index')
 @app.route('/home')
 def index():
@@ -40,9 +81,15 @@ def index():
 def datasheets():
 	return render_template('datasheets.html', title='Datasheet')
 
-@app.route('/details')
+@app.route('/details', methods = ['POST', 'GET'])
 def details():
-	return render_template('details.html', title='Details')
+
+	if request.method == 'POST':
+		temp = request.form['Temperature']
+		humi = request.form['Humidity']
+		return render_template('details.html', posts=posts, title='Details', value=temp)
+	elif request.method == 'GET':
+		return render_template('details.html', posts=posts, title='Details')
 
 @app.route('/about')
 def about():
@@ -54,10 +101,41 @@ def jsontest():
 	humi = request.form['Humidity']
 
 	if humi=="100":
-		
+
 		return "humidity fine"
 	elif humi=="200":
-		return "humidity high!"
+		return"humidity high!"
+
+#Test Stuff-----
+
+@app.route('/_stuff', methods = ['POST', 'GET'])
+def stuff():
+    #return jsonify(result=time.time())
+	global temp, humi, co, co2, smoke, pressure
+
+	if request.method == 'POST':
+		temp = request.form['Temperature']
+		humi = request.form['Humidity']
+		co = request.form['Co']
+		co2 = request.form['Co2']
+		smoke = request.form['Smoke']
+		pressure = request.form['Pressure']
+		return jsonify(result=temp)
+		#return "Posted!"
+	elif request.method == 'GET':
+		
+		return jsonify(result=temp, result1=humi, result2=pressure, result3=co2, result4=smoke, result5=co)  
+
+
+@app.route('/save-get',methods=['POST', 'GET'])
+def saveget():
+    if request.method=='GET':
+       a=request.args.get('Temperature', '')
+       b=request.args.get('Humidity', '')
+       return "Temperature : "+a+" ,  Humidity :  "+a
+    else:
+        return "Not get method"
+#-----------------------
 
 if __name__ == '__main__':
 	app.run(debug=True)
