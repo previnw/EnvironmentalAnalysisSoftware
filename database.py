42a82c8f4404eb56d5d6ec1726b097d34c076632
DanPilki <Dan.pilkington@me.com>
2018-11-24 17:31:06 -0800

    updates on db

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 import sqlite3
 from index import Temperature, CarbonMonoxide, CarbonDioxide, Humidity
 
-conn = sqlite3.connect('data.db')   #change this to :memory: for testing
+conn = sqlite3.connect(':memory:')   #change this to :memory: for testing
 									#change to data.db for application, will add file into directory
 
 c = conn.cursor()
@@ -32,7 +32,7 @@ c.execute("""CREATE TABLE humidity (
 
 temp_write = Temperature(200, "temperature", 1)
 
-#c.execute("INSERT INTO temperature VALUES (:id, :reading)", {'id': temp_write.id_temp, 'reading': temp_write.data_val_temp})
+c.execute("INSERT INTO temperature VALUES (:id, :reading, :type)", {'id': temp_write.id_temp, 'reading': temp_write.data_val_temp, 'type': temp_write.type_temp})
 
 conn.commit()
 
