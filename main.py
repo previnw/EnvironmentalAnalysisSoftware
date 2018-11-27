473d6975dc0fccb4bfb0ef72c0314e1ab4ed5c44
DanPilki <Dan.pilkington@me.com>
2018-11-26 20:54:50 -0800

    fully functional DB, deletes at 100

--- main.py
+++ main.py
@@ -1,37 +1,43 @@
 # terminal command to test and send data to app
 # curl --request POST --url https://go-environment.herokuapp.com/_stuff --data "Temperature=1&Humidity=2&Co=3&Co2=4&Smoke=5&Pressure=6"
+# curl --request POST --url localhost:5000/_stuff --data "Temperature=1&Humidity=2&Co=3&Co2=4&Smoke=5&Pressure=6"
 
 from flask import Flask, render_template, url_for, jsonify, request, redirect
-from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, DecimalField
 import datetime, json
+import sqlite3
+
+conn = sqlite3.connect('data.db') 
+
+c = conn.cursor()
 
 class Temperature:
-	def __init__(self, data_val_temp, type_temp, id_temp):
-		self.id_temp = id_temp
-		self.type_temp = type_temp
+	def __init__(self, data_val_temp):
 		self.data_val_temp = data_val_temp
 
 class CarbonDioxide:
-	def __init__(self, data_val_CO2, type_CO2, id_CO2):
-		self.id_CO2 = id_CO2
-		self.type_CO2 = type_CO2
+	def __init__(self, data_val_CO2):
 		self.data_val_CO2 = data_val_CO2
 
 class CarbonMonoxide:
-	def __init__(self, data_valCO, type_CO, id_CO):
-		self.id_CO = id_CO
-		self.type_CO = type_CO
+	def __init__(self, data_valCO):
 		self.data_valCO = data_valCO
 
 class Humidity:
-	def __init__(self, data_val_hum, type_hum, id_hum):
-		self.id_hum = id_hum
-		self.type_hum = type_hum
+	def __init__(self, data_val_hum):
 		self.data_val_hum = data_val_hum
 
-global temp, humi, co, co2, smoke, pressure
+class Pressure_C:
+	def __init__(self, data_val_pres):
+		self.data_val_pres = data_val_pres
+
+class Smoke_C:
+	def __init__(self, data_val_smo):
+		self.data_val_smo = data_val_smo
+
+global temp, humi, co, co2, smoke, pressure, POS
+POS = 0
 temp = 0
 humi = 0
 co = 0
@@ -81,15 +87,34 @@ def jsontest():
 @app.route('/_stuff', methods = ['POST', 'GET'])
 def stuff():
     #return jsonify(result=time.time())
-	global temp, humi, co, co2, smoke, pressure
+	global temp, humi, co, co2, smoke, pressure, POS
 
 	if request.method == 'POST':
+		conn = sqlite3.connect('data.db') 
+		c = conn.cursor()
 		temp = request.form['Temperature']
+		c.execute("INSERT INTO temperature VALUES (:reading, NULL)", {'reading': float(temp)})
 		humi = request.form['Humidity']
+		c.execute("INSERT INTO humidity VALUES (:reading, NULL)", {'reading': float(humi)})
 		co = request.form['Co']
+		c.execute("INSERT INTO carbonMonoxide VALUES (:reading, NULL)", {'reading': float(co)})
 		co2 = request.form['Co2']
+		c.execute("INSERT INTO carbonDioxide VALUES (:reading, NULL)", {'reading': float(co2)})
 		smoke = request.form['Smoke']
+		c.execute("INSERT INTO smoke_table VALUES (:reading, NULL)", {'reading': float(smoke)})
 		pressure = request.form['Pressure']
+		c.execute("INSERT INTO pressure_table VALUES (:reading, NULL)", {'reading': float(pressure)})
+		if POS > 100:
+			c.execute("DELETE FROM temperature")
+			c.execute("DELETE FROM humidity")
+			c.execute("DELETE FROM smoke_table")
+			c.execute("DELETE FROM carbonDioxide")
+			c.execute("DELETE FROM carbonMonoxide")
+			c.execute("DELETE FROM pressure_table")
+			POS = 0
+		POS += 1
+		conn.commit()
+		conn.close()
 		return jsonify(result=temp)
 		#return "Posted!"
 	elif request.method == 'GET':
@@ -109,4 +134,3 @@ def saveget():
 
 if __name__ == '__main__':
 	app.run(debug=True)
-
